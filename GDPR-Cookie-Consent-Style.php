5901cbe6b36d85822c7bf7f8454b4f6a14d3d43f
client.studio <hello@client.fi>
2019-09-10 19:24:26 +0300

    Create GDPR-Cookie-Consent-Style.php

---
+++ GDPR-Cookie-Consent-Style.php
@@ -0,0 +1,5 @@
+<!-- insert ths to Cookie Law Settings > Message -->
+
+<div class="container-fluid"><div class="row py-2">
+<div class="text-center text-md-left px-3 col-12 col-md-10 d-flex align-items-center"><p class="mb0">🍪 This website uses cookies to improve your experience. We'll assume you're ok with this, but you can opt-out if you wish. [cookie_link margin="0px"]</p></div>
+<div class="col-12 col-md-2 text-md-right"> [cookie_button margin="10px"]</div></div></div>
