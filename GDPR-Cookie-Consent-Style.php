67c3a6d2303b060b87cb39224dc4085952c733f0
client.studio <hello@client.fi>
2020-08-04 12:18:28 +0300

    Update GDPR-Cookie-Consent-Style.php

--- GDPR-Cookie-Consent-Style.php
+++ GDPR-Cookie-Consent-Style.php
@@ -1,6 +1,6 @@
 <!-- https://wordpress.org/plugins/cookie-law-info/ -->
 <!-- insert to Cookie Law Settings -> Message -->
 
-<div class="container-fluid"><div class="row py-2">
-<div class="text-center text-md-left px-3 col-12 col-md-10 d-flex align-items-center"><p class="pb-0">🍪 This website uses cookies to improve your experience. We'll assume you're ok with this, but you can opt-out if you wish. [cookie_link margin="0px"]</p></div>
-<div class="col-12 col-md-2 text-center text-md-right"> [cookie_button margin="10px"]</div></div></div>
+<div class="container-fluid"><div class="row py-2 d-flex align-items-center">
+<div class="text-center text-md-left px-3 col-12 col-md-10"><p class="pb-0 mb-0">🍪 This website uses cookies to improve your experience. We'll assume you're ok with this, but you can opt-out if you wish. [cookie_link margin="0px"]</p></div>
+<div class="col-12 col-md-2 text-md-right"> [cookie_button margin="10px"]</div></div></div>
